507ea74252b0f9bb969839bc6d93266d55cf2510
roniboden <roni2818@gmail.com>
2026-01-11 12:48:52 +0200

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true // Automatically open the browser
-  }
+    port: 3000, 
+    open: true,
+    proxy: {
+      '/auth': 'http://localhost:5000',
+      '/reports': 'http://localhost:5000',
+      '/ducks': 'http://localhost:5000',
+      '/images': 'http://localhost:5000',
+    },
+  },
 })
